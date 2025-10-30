2f39f58e50b03551215c9aceb15decfb1f4e68b0
Emilio <xxglobalelitexx123@gmail.com>
2025-10-30 14:09:36 +0100

    Added: Comments to tcp_read

--- src/tcp/tcp.h
+++ src/tcp/tcp.h
@@ -3,6 +3,12 @@
 
 #include <stdio.h>
 
+
+/*
+    Reads whole message from socket and returns it as char* with its whole correct size (no memory above what is needed is used)
+    
+    WARNING: The returned value needs to be freed
+*/
 char* tcp_read(int socket_to_read_from, size_t max_buffer_len);
 
 
